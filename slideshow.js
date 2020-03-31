221b82b2da2d33badb83064582dbd081581cd3fb
Hector Gonzalez <hectarek13@gmail.com>
2020-03-31 15:03:17 -0400

    add show photo on next and prev

--- slideshow.js
+++ slideshow.js
@@ -11,8 +11,10 @@ var slideshow = {
         if (this.currentPhotoIndex > this.photoList.length - 1 ) {
             this.currentPhotoIndex = 0;
             console.log('Back to beginning of slideshow')
+            console.log('Photo is:', this.photoList[this.currentPhotoIndex]);
         } else {
             console.log('next slide');
+            console.log('Photo is:', this.photoList[this.currentPhotoIndex]);
         };
     },
     prevPhoto: function() {
@@ -22,8 +24,12 @@ var slideshow = {
         if (this.currentPhotoIndex < 0 ) {
             this.currentPhotoIndex = this.photoList.length - 1 
             console.log('Back to end of slideshow');
+            console.log('Photo is:', this.photoList[this.currentPhotoIndex]);
+
         } else {
             console.log('previous slide');
+            console.log('Photo is:', this.photoList[this.currentPhotoIndex]);
+
         }
     },
     getCurrentPhoto: function() {
