736b9b86a0cb9e39b2a4787b1b4e70a739ac04b9
Hector Gonzalez <hectarek13@gmail.com>
2020-04-01 14:23:01 -0400

    Finished with Async functions

--- slideshow.js
+++ slideshow.js
@@ -6,43 +6,42 @@ var slideshow = {
     currentPhotoIndex: 0,
     nextPhoto: function() {
 
-        this.currentPhotoIndex++;
-
-        if (this.currentPhotoIndex > this.photoList.length - 1 ) {
-            this.currentPhotoIndex = -1;
-            console.log('End of Slideshow')
+        if (this.currentPhotoIndex < this.photoList.length - 1 ) {
+            this.currentPhotoIndex++;   
+            console.log("Current Photo is: ", this.photoList[this.currentPhotoIndex]);
         } else {
-            console.log('next slide');
-            console.log('Photo is:', this.photoList[this.currentPhotoIndex]);
+            console.log('End of Slideshow');
+            this.pause()
         };
     },
     prevPhoto: function() {
         
-        this.currentPhotoIndex--;
-
-        if (this.currentPhotoIndex < 0 ) {
-            this.currentPhotoIndex = this.photoList.length
-            console.log('Beginning of Slideshow');
+        if (this.currentPhotoIndex > 0) {
+            this.currentPhotoIndex--;
+            console.log('Current Photo is: ', this.photoList[this.currentPhotoIndex]);
         } else {
-            console.log('previous slide');
-            console.log('Photo is:', this.photoList[this.currentPhotoIndex]);
-
+            console.log('Beginning of Slideshow');
         }
     },
     getCurrentPhoto: function() {
-        console.log('Current Photo is:', this.photoList[this.currentPhotoIndex]);
+       return this.photoList[this.currentPhotoIndex];
+
+    },
+    //Async Changes
+    playInterval: null,
+
+    play: function(){
+
+        this.playInterval = setInterval(function() {
+            this.nextPhoto();
+        }.bind(this), 2000)
+
+    },
+    pause: function(){
+        clearInterval(this.playInterval)
     }
+
 }
 
-// Async changes
-
-slideshow.getCurrentPhoto();
-slideshow.prevPhoto();
-slideshow.prevPhoto();
-slideshow.nextPhoto();
-slideshow.nextPhoto();
-slideshow.nextPhoto();
-slideshow.nextPhoto();
-slideshow.getCurrentPhoto();
-
+slideshow.play();
 
